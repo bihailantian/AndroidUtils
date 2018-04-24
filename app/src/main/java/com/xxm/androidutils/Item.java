23340798e8da1055af7dcc4911b5a38288b6435f
xuxiaoming <1229717912@qq.com>
2018-04-24 10:06:56 +0800

    Initial commit

---
+++ app/src/main/java/com/xxm/androidutils/Item.java
@@ -0,0 +1,30 @@
+package com.xxm.androidutils;
+
+
+public class Item {
+
+    private String title;
+    private String msg;
+
+    public Item(String title, String msg) {
+        this.title = title;
+        this.msg = msg;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
---
+++ app/src/main/java/com/xxm/androidutils/MainActivity.java
@@ -0,0 +1,83 @@
+package com.xxm.androidutils;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Context mContext;
+    private List<Item> itemList = new ArrayList<>();
+
+    {
+        itemList.add(new Item("标题", "内容"));
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mContext = this;
+        ListView mListView = findViewById(R.id.listView);
+        mListView.setAdapter(new ListAdapter());
+
+    }
+
+
+    class ListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return itemList.size();
+        }
+
+        @Override
+        public Item getItem(int position) {
+            return itemList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+            if (convertView == null) {
+                convertView = View.inflate(mContext, R.layout.list_item, null);
+                holder = new ViewHolder();
+                holder.tvTitle = convertView.findViewById(R.id.tv_title);
+                holder.tvMsg = convertView.findViewById(R.id.tv_msg);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            Item item = getItem(position);
+            holder.tvTitle.setText(item.getTitle());
+            holder.tvMsg.setText(item.getMsg());
+
+            return convertView;
+        }
+
+
+        class ViewHolder {
+            TextView tvTitle;
+            TextView tvMsg;
+        }
+
+    }
+
+
+}
