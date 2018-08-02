0a80006e44d48cda181e02d6ab21a20468bab6f5
xuxiaoming <1229717912@qq.com>
2018-08-02 15:16:42 +0800

    添加判断应用是处于前还是后台方法

--- app/src/main/java/com/xxm/androidutils/utils/AppUtils.java
+++ app/src/main/java/com/xxm/androidutils/utils/AppUtils.java
@@ -1,9 +1,13 @@
 package com.xxm.androidutils.utils;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.util.Log;
+
+import java.util.List;
 
 /**
  * 跟App相关的辅助类
@@ -51,4 +55,34 @@ public class AppUtils {
         }
         return null;
     }
+
+
+    /**
+     * 判断应用是处于前还是后台，处于前台返回false，处于后台返回true
+     *
+     * @param context 上下文
+     */
+    public static boolean isBackground(Context context) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> appProcesses = activityManager.getRunningAppProcesses();
+        for (ActivityManager.RunningAppProcessInfo appProcess : appProcesses) {
+            if (appProcess.processName.equals(context.getPackageName())) {
+                /*
+                BACKGROUND=400 EMPTY=500 FOREGROUND=100
+                GONE=1000 PERCEPTIBLE=130 SERVICE=300 ISIBLE=200
+                 */
+                Log.i(context.getPackageName(), "此app importance =" + appProcess.importance + ",context.getClass().getName()=" + context.getClass().getName());
+                if (appProcess.importance != ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                    Log.i(context.getPackageName(), "处于后台" + appProcess.processName);
+                    return true;
+                } else {
+                    Log.i(context.getPackageName(), "处于前台" + appProcess.processName);
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+
 }
